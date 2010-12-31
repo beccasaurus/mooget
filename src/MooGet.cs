4d79fc8f986d4cbc9440c48302853a6fd93d4b8c
remi <remi@remitaylor.com>
2010-12-31 14:47:46 -0700

    moo config

--- src/MooGet.cs
+++ src/MooGet.cs
@@ -4,8 +4,25 @@ namespace MooGet {
 
 	/// <summary>The command line runner</summary>
 	public class Runner {
+
+		// We'll refactor to using a nice option parser when we need to ... we'll keep is stupid simple until then
 		public static void Main(string[] args) {
-			Console.WriteLine("moo");
+			if (args.Length == 0)
+				Console.WriteLine("moo");
+			else {
+				switch (args[0]) {
+					case "config":
+						PrintConfig();
+						break;
+					default:
+						Console.WriteLine("Unknown command: {0}", args[0]);
+						break;
+				}
+			}
+		}
+
+		static void PrintConfig() {
+			Console.WriteLine("mooDir: {0}", Util.HomeDirectory);
 		}
 	}
 }
