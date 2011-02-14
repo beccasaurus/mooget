85a6545bdbbae6ff2e252c232c17c81490ac229a
remi <remi@remitaylor.com>
2011-02-14 00:34:38 -0700

    cow command is now Debug, so it doesn't show up in the main list.  :)

--- src/cli/commands/CowCommand.cs
+++ src/cli/commands/CowCommand.cs
@@ -5,7 +5,7 @@ namespace MooGet.Commands {
 	/// <summary>moo cow</summary>
 	public class CowCommand {
 
-		[Command(Name = "cow", Description = "Moo.")]
+		[Command("cow", "Moo.", Debug = true)]
 		public static object Run(string[] args) {
 			if (args.Length == 0)
 				args = new string[] { "Moo" };
