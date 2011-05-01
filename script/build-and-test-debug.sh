8392bbf472b919ea7e9945dd5a28c7d16d06da5d
remi <remi@remitaylor.com>
2011-05-01 13:50:19 -0700

    New build scripts

--- script/build-and-test-debug.sh
+++ script/build-and-test-debug.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 find . -name obj -type d -exec rm -rfv {} \;
 find . -name bin -type d -exec rm -rfv {} \;
+rm -rf TestResult.xml
+xbuild
+nunit-color-console -labels "$@" bin/Debug/MooGet.Specs.dll
---
+++ script/build-and-test-release.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+find . -name obj -type d -exec rm -rfv {} \;
+find . -name bin -type d -exec rm -rfv {} \;
+rm -rf TestResult.xml
+xbuild /p:Configuration=Release
+nunit-color-console -labels "$@" bin/Release/MooGet.Specs.dll
